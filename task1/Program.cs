b967dc9c237ddb9ff473823df76b374118bdda36
Skizex <justskizex@gmail.com>
2023-11-06 18:35:09 +0400

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -8,10 +8,10 @@ if (b<1)
 Console.Write("Число B должно быть натуральным\n");
 else
 {
-while (b!=0)
-{
-    c = c * a;
-    b = b - 1;
-}
-Console.Write($"Число A в натуральной степени B равно {c}");
+    while (b!=0)
+    {
+        c = c * a;
+        b = b - 1;
+    }
+    Console.Write($"Число A в натуральной степени B равно {c}");
 }
